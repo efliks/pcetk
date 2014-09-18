41f7e03c3a77b5bea2c7283d5f56fe26342cc89f
mikolaj.feliks@gmail.com <mikolaj.feliks@gmail.com@ee7e1ac3-3b4a-1822-f870-9d0fbee2ef6a>
2014-09-18 16:44:03 +0000

    Writing XTC trajectories should work now.

--- trunk/ContinuumElectrostatics/ContinuumElectrostatics/Site.py
+++ trunk/ContinuumElectrostatics/ContinuumElectrostatics/Site.py
@@ -27,13 +27,51 @@ class MEADSite (object):
                   "siteAtomIndices"  : None ,
                       }
 
+  @property
+  def label (self): return "%s_%s%s" % (self.segName, self.resName, self.resSerial)
+
+
   def __init__ (self, *arguments, **keywordArguments):
     """Constructor."""
     for (key, value) in self.__class__.defaultAttributes.iteritems (): setattr (self, key, value)
     for (key, value) in                 keywordArguments.iteritems (): setattr (self, key, value)
 
-  @property
-  def label (self): return "%s_%s%s" % (self.segName, self.resName, self.resSerial)
+
+  def GetMostProbableInstance (self):
+    """Return the index, label and probability of the most probable instance of a site."""
+    model = self.parent
+    if not model.isProbability:
+      raise ContinuumElectrostaticsError ("First calculate probabilities.")
+
+    mostProbValue = 0.
+    for instance in self.instances:
+      if instance.probability > mostProbValue:
+        mostProbValue = instance.probability
+        mostProbIndex = instance.instIndex
+        mostProbLabel = instance.label
+    #instances = []
+    #for instance in self.instances:
+    #  instances.append ([instance.probability, instance.instIndex, instance.label])
+    #
+    #instances.sort ()
+    #mostProbValue, mostProbIndex, mostProbLabel = instances[-1]
+
+    return (mostProbValue, mostProbIndex, mostProbLabel)
+
+
+  def GetSortedIndices (self):
+    """Get a list of indices of instances sorted by increasing probability."""
+    model = self.parent
+    if not model.isProbability:
+      raise ContinuumElectrostaticsError ("First calculate probabilities.")
+
+    instances = []
+    for instance in self.instances:
+      instances.append ([instance.probability, instance.instIndex])
+
+    instances.sort ()
+    indices = [index for probability, index in instances]
+    return indices 
 
 
 #===============================================================================
